c082bc279935a852977b4800df33bd4557b838fd
Wei Jen Lu <weijenlu@gmail.com>
2013-06-19 19:39:27 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gurupu::Application.config.secret_key_base = 'f7472106de804115b138dd0af0744699e92e8cedadab0ea97295c0ed62503b06066201e4308fc32f9d001956acfde86657f0d6bc0dfdd9230b4228f9102f15eb'
