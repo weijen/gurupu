98722769360b34e46dfd78444c44f5a0cc9f7c08
macalgd <macalgd@gmail.com>
2013-06-29 13:46:20 +0800

    fix group index current_user

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,7 +2,7 @@ class GroupsController < ApplicationController
   before_action :set_group, only: [:show, :edit, :update, :destroy]
 
   def index
-    @groups=@current_user.groups #maca
+    @groups=current_user.groups #maca
     #maca @groups = Group.all
   end
 
